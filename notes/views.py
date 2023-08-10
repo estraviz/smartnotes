d9b34b57be9c1cd4714aa408eb362f0d8d31e571
Javier Estraviz <javier.estraviz@gmail.com>
2023-08-10 22:04:06 +0200

    Added form to create a new note

--- notes/views.py
+++ notes/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import Http404
-from django.views.generic import DetailView, ListView
+from django.views.generic import CreateView, DetailView, ListView
 
 from .models import Notes
+
+class NotesCreateView(CreateView):
+    model = Notes
+    fields = ['title', 'text']
+    success_url = '/smart/notes'
 
 class NotesListView(ListView):
     model = Notes
@@ -12,14 +17,3 @@ class NotesListView(ListView):
 class NotesDetailView(DetailView):
     model = Notes
     context_object_name ='note'
-
-def list(request):
-    all_notes = Notes.objects.all()
-    return render(request, 'notes/notes_list.html', {'notes': all_notes})
-
-def detail(request, pk):
-    try:
-        note = Notes.objects.get(pk=pk)
-    except Notes.DoesNotExist:
-        raise Http404("This note doesn't exist")
-    return render(request, 'notes/notes_detail.html', {'note': note})
