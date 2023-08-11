8c9efc6452e44484ad6fca31fce369e0ad3d7eab
Javier Estraviz <javier.estraviz@gmail.com>
2023-08-11 23:14:51 +0200

    Added signup

--- home/views.py
+++ home/views.py
@@ -1,11 +1,22 @@
-from django.shortcuts import render
-from django.http import HttpResponse
 from datetime import datetime
-from django.contrib.auth.decorators import login_required
-from django.views.generic import TemplateView
+
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import TemplateView
+from django.views.generic.edit import CreateView
 
+from django.shortcuts import redirect
+
+class SignupView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'home/register.html'
+    success_url = '/smart/notes'
+
+    def get(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('notes.list')
+        return super().get(request, *args, **kwargs)
 
 class LogoutInterfaceView(LogoutView):
     template_name = 'home/logout.html'
@@ -20,10 +31,3 @@ class HomeView(TemplateView):
 class AuthorizedView(LoginRequiredMixin, TemplateView):
     template_name = 'home/authorized.html'
     login_url = '/admin'
-
-def home(request):
-    return render(request, 'home/welcome.html', {'today': datetime.today()})
-
-@login_required(login_url='/admin')
-def authorized(request):
-    return render(request, 'home/authorized.html', {})
